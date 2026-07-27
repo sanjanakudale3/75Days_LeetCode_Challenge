05212f30be4f815c5986d441c670d8bf908ac497
Sanjana Kudale <sanjanakudale3@gmail.com>
2026-07-27 11:58:17 +0530

    Time: 10 ms (27.47%), Space: 47.4 MB (6.9%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        Arrays.sort(nums);
+        for(int i = 0 ; i<nums.length-1; i++){
+            if(nums[i]==nums[i+1]){
+            i++;
+            }else{
+                return nums[i];
+            }
+            
+        }
+        return nums[nums.length-1];
+    
+}
+}

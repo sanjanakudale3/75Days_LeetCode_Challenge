5981e4034838fa350a507d3b9399fb743b92b00f
Sanjana Kudale <sanjanakudale3@gmail.com>
2026-09-23 10:29:24 +0530

    Time: 0 ms (100%), Space: 46.9 MB (13.85%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,30 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
 
-        ListNode slow = head;
+        ListNode slow = head; 
         ListNode fast = head;
 
-        // Step 1: Detect cycle
-        while (fast != null && fast.next != null) {
-
-            slow = slow.next;
+        while( fast != null && fast.next != null){
+            slow = slow.next ;
             fast = fast.next.next;
-
-            if (slow == fast) {
-
-                // Step 2: Find starting point
+            
+            
+            if(slow == fast){
                 slow = head;
+            
 
-                while (slow != fast) {
-                    slow = slow.next;
-                    fast = fast.next;
-                }
-
-                return slow;
+            while( slow != fast){
+                slow = slow.next;
+                fast = fast.next;
             }
+            return slow;
+        }
         }
 
-        // No cycle
-        return null;
+    return null;
+        
     }
 }
